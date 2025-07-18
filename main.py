2735ef81c8379646804ed9bdc608f32a268a21de
Ankit Gupta <aryangupta2224@gmail.com>
2025-07-18 21:55:32 +0530

    Add bot functionality and WebRTC integration
    - Introduced a new bot module (`bot.py`) that implements a Gemini Chatbot using Pipecat for audio processing and WebRTC for real-time communication.
    - Updated `main.py` to handle WebRTC connections and integrate the bot, allowing for dynamic connection management and background task execution.
    - Enhanced `requirements.txt` to include necessary dependencies for the new bot functionality and WebRTC support.

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
-from fastapi import FastAPI
 
+import argparse
+import asyncio
+import sys
+from contextlib import asynccontextmanager
+from typing import Dict
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import BackgroundTasks, FastAPI
+from fastapi.responses import FileResponse
+from loguru import logger
+from bot import run_bot
+from pipecat.transports.network.webrtc_connection import IceServer, SmallWebRTCConnection
+
+pcs_map:Dict[str,SmallWebRTCConnection] = {}
 app = FastAPI()
+ice_servers = [
+    IceServer(
+        urls="stun:stun.l.google.com:19302",
+    )
+]
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI with Docker!"} 
 
-@app.get("/create-offer")
-def handle_create_offer():
-    return {"message": "Create offer"}
+
+
+@app.post("/api/offer")
+async def offer(request: dict, background_tasks: BackgroundTasks):
+    pc_id = request.get("pc_id")
+
+    if pc_id and pc_id in pcs_map:
+        pipecat_connection = pcs_map[pc_id]
+        logger.info(f"Reusing existing connection for pc_id: {pc_id}")
+        await pipecat_connection.renegotiate(sdp=request["sdp"], type=request["type"])
+    else:
+        pipecat_connection = SmallWebRTCConnection(ice_servers)
+        await pipecat_connection.initialize(sdp=request["sdp"], type=request["type"])
+
+        @pipecat_connection.event_handler("closed")
+        async def handle_disconnected(webrtc_connection: SmallWebRTCConnection):
+            logger.info(f"Discarding peer connection for pc_id: {webrtc_connection.pc_id}")
+            pcs_map.pop(webrtc_connection.pc_id, None)
+
+        background_tasks.add_task(run_bot, pipecat_connection)
+
+    answer = pipecat_connection.get_answer()
+    # Updating the peer connection inside the map
+    pcs_map[answer["pc_id"]] = pipecat_connection
+
+    return answer
